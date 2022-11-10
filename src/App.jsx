69c838f2cd58e3f1d6965488885229ccfd893126
Ren Amaki <jorge_luis1709@hotmail.com>
2022-11-10 12:44:32 -0600

    first commit, development enviroment is ready

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="w-full h-screen bg-indigo-400 text-6xl text-white font-bold text-center leading-[100vh] align-middle">Hola tailwind</div>;
+};
+
+export default App;
