bdfd99372e0da2d752b3d9e6182de1602c1109cd
Ren Amaki <jorge_luis1709@hotmail.com>
2022-11-15 18:23:31 -0600

    ajustes a la pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -30,9 +30,9 @@ const App = () => {
   } = useApp();
 
   return (
-    <main className={`absolute ${ darkMode && "dark"}`}>
+    <main className={`absolute ${ darkMode && "dark"} w-full`}>
       <button
-        className="fixed top-mobileRangeSpacing left-mobileRangeSpacing px-mobileRangeSpacing py-3 w-auto h-auto text-mobileRange text-slate-500 rounded-full shadow-md z-10 hover:bg-zinc-200 active:bg-zinc-300 dark:shadow-zinc-500 dark:text-zinc-200 dark:hover:bg-zinc-900/60 dark:active:bg-zinc-900"
+        className="absolute top-mobileRangeSpacing left-mobileRangeSpacing px-mobileRangeSpacing py-3 w-auto h-auto text-mobileRange text-slate-500 rounded-full shadow-md z-10 hover:bg-zinc-200 active:bg-zinc-300 dark:shadow-zinc-500 dark:text-zinc-200 dark:hover:bg-zinc-900/60 dark:active:bg-zinc-900"
         onClick={() => {
           setDarkMode((state) => !state);
         }}
